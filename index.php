76f8d95901c5016829a8418ef35348fe44a649f4
Cardenas2915 <eduar.cardenas15@gmail.com>
2023-09-27 11:55:26 -0500

    se realizaron funciones para subir el archivo JSON y dibujar las coordenadas dependiendo de las ubicaciones

--- index.php
+++ index.php
@@ -21,10 +21,10 @@ if(isset($_SESSION['login'])){
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.css" rel="stylesheet" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
 </head>
-<body class="bg-[url('img/BG.png')] bg-cover">
+<body class="bg-green-50">
 
 <!-- Botones de apertura de modal -->
-<div class="flex justify-evenly items-center gap-2 m-3">
+<div class="flex justify-evenly items-center gap-2 my-6">
     <?php if(isset($_SESSION['login'])): ?>
         <p class="font-medium text-ls px-5 py-2.5 text-center">Bienvenido! <?= $datos['nombre'] ?></p>
     <?php endif; ?>
@@ -106,10 +106,10 @@ if(isset($_SESSION['login'])){
                 <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Sube tu archivo JSON</h3>
                 <form class="space-y-6" id="formulariologgin">
                     <div>
-                        <input type="file" id="usuario" name="archivo" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white rounded" required>
+                        <input type="file" id="fileInput" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white rounded" required>
                     </div>
 
-                    <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" id="EmpezarJuego">Cargar Archivo</button>
+                    <button id="loadButton" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" >Cargar Archivo</button>
                 </form>
             </div>
         </div>
@@ -178,7 +178,7 @@ if(isset($_SESSION['login'])){
         </div>
     </div>
 </div>
-<section class="h-screen flex justify-center items-center" id="mapa"></section>
+<div class="bg-[url('img/BG.png')] bg-cover flex m-auto h-[30rem] w-[70rem] border-slate-500 rounded-xl border-2" id="mapa"></div>
 
 </body>
 </html>
--- js/rutas.js
+++ js/rutas.js
@@ -1,121 +1,122 @@
-const data = {
-"ubicaciones": [
-    {
-    "nombre": "A",
-    "posX": 20,
-    "posY": 20
-    },
-    {
-    "nombre": "B",
-    "posX": 45,
-    "posY": 60
-    },
-    {
-    "nombre": "C",
-    "posX": 79,
-    "posY": 90
-    },
-    {
-    "nombre": "D",
-    "posX": 56,
-    "posY": 79
-    },
-    {
-    "nombre": "E",
-    "posX": 86,
-    "posY": 19
+//tomamos los vaores JSON que se guardaron en el localStorage
+const JsonStorage = localStorage.getItem('datos');
+const datosJSON = JSON.parse(JsonStorage);
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    //llamado de las funciones
+    mostrarELemento(datosJSON);
+})
+
+//se recibe y valida el archivo JSON que se cargo en el formulario
+document.getElementById('loadButton').addEventListener('click', function () {
+    const fileInput = document.getElementById('fileInput');
+    // Verifica si se seleccionó un archivo
+    if (fileInput.files.length === 0) {
+        alert('Por favor, seleccione un archivo JSON.');
+        return;
     }
-],
-"conexiones": [
-    {
-    "ubicacion1": "A",
-    "ubicacion2": "B",
-    "peso": 20
-    },
-    {
-    "ubicacion1": "C",
-    "ubicacion2": "D",
-    "peso": 50
-    },
-    {
-    "ubicacion1": "A",
-    "ubicacion2": "C",
-    "peso": 70
-    },
-    {
-    "ubicacion1": "D",
-    "ubicacion2": "E",
-    "peso": 10
-    },
-    {
-    "ubicacion1": "B",
-    "ubicacion2": "E",
-    "peso": 80
-    },
-    {
-    "ubicacion1": "A",
-    "ubicacion2": "D",
-    "peso": 5
+    const selectedFile = fileInput.files[0];
+    // Verifica si el archivo seleccionado es un archivo JSON
+    if (selectedFile.type !== 'application/json') {
+        alert('El archivo seleccionado no es un archivo JSON válido.');
+        return;
     }
-    
-]
-};
-
-function algoritmo(data, inicio) {
-const ubicaciones = data.ubicaciones;
-const conexiones = data.conexiones;
-
-const distancia = {};
-const previo = {};
-const nodosNoVisitados = new Set();
-
-ubicaciones.forEach(ubicacion => {
-    distancia[ubicacion.nombre] = Infinity;
-    previo[ubicacion.nombre] = undefined;
-    nodosNoVisitados.add(ubicacion.nombre);
-});
-
-distancia[inicio] = 0;
-
-while (nodosNoVisitados.size > 0) {
-    let nodoActual = null;
-    nodosNoVisitados.forEach(ubicacion => {
-    if (!nodoActual || distancia[ubicacion] < distancia[nodoActual]) {
-        nodoActual = ubicacion;
-    }
-    });
-
-    nodosNoVisitados.delete(nodoActual);
-
-    conexiones.forEach(conexion => {
-    if (conexion.ubicacion1 === nodoActual) {
-        const vecino = conexion.ubicacion2;
-        const pesoTotal = distancia[nodoActual] + conexion.peso;
-        if (pesoTotal < distancia[vecino]) {
-        distancia[vecino] = pesoTotal;
-        previo[vecino] = nodoActual;
+    const reader = new FileReader();
+    reader.onload = function (event) {
+        try {
+        const datosJSON = JSON.parse(event.target.result);
+        // Aquí puedes trabajar con el contenido JSON, por ejemplo, mostrarlo en la consola
+        const jsonString = JSON.stringify(datosJSON);
+        localStorage.setItem('datos', jsonString);
+        } catch (error) {
+        alert('Error al analizar el archivo JSON.');
         }
-    }
+    };
+    reader.readAsText(selectedFile);
     });
+
+
+
+
+// funciones
+function mostrarELemento(archivoJson){
+    const {ubicaciones} = archivoJson
+    ubicaciones.forEach(obj => {
+        const {nombre,posX, posY} = obj;
+        crearPunto(nombre,posX,posY);
+    })
+    
+}
+function crearPunto(nombre, posX, posY) {
+    const mapa = document.getElementById('mapa');
+
+        const nombrePos = document.createElement('p')
+        nombrePos.textContent = `${nombre}`
+        nombrePos.classList.add('text-sm','mt-2')
+
+        const puntoRojo = document.createElement('div');
+            puntoRojo.className = 'red-dot';
+            puntoRojo.style.left = posX + 'px';
+            puntoRojo.style.top = posY + 'px';
+        puntoRojo.appendChild(nombrePos)
+        mapa.appendChild(puntoRojo);
 }
 
-// Construir la ruta más corta
-const ruta = [];
-let nodo = inicio;
-while (previo[nodo] !== undefined) {
-    ruta.unshift(nodo);
-    nodo = previo[nodo];
+
+function mostrarRuta(data, inicio) {
+    const ubicaciones = data.ubicaciones;
+    const conexiones = data.conexiones;
+
+    const distancia = {};
+    const previo = {};
+    const nodosNoVisitados = new Set();
+
+    ubicaciones.forEach(ubicacion => {
+        distancia[ubicacion.nombre] = Infinity;
+        previo[ubicacion.nombre] = undefined;
+        nodosNoVisitados.add(ubicacion.nombre);
+    });
+
+    distancia[inicio] = 0;
+
+    while (nodosNoVisitados.size > 0) {
+        let nodoActual = null;
+        nodosNoVisitados.forEach(ubicacion => {
+        if (!nodoActual || distancia[ubicacion] < distancia[nodoActual]) {
+            nodoActual = ubicacion;
+        }
+        });
+
+        nodosNoVisitados.delete(nodoActual);
+
+        conexiones.forEach(conexion => {
+        if (conexion.ubicacion1 === nodoActual) {
+            const vecino = conexion.ubicacion2;
+            const pesoTotal = distancia[nodoActual] + conexion.peso;
+            if (pesoTotal < distancia[vecino]) {
+            distancia[vecino] = pesoTotal;
+            previo[vecino] = nodoActual;
+            }
+        }
+        });
+    }
+
+    // Construir la ruta más corta
+    const ruta = [];
+    let nodo = inicio;
+    while (previo[nodo] !== undefined) {
+        ruta.unshift(nodo);
+        nodo = previo[nodo];
+    }
+
+    ruta.unshift(inicio);
+
+    return {
+        distancia: distancia,
+        ruta: ruta // Cambiamos esto de rutasUtilizadas a ruta
+    };
 }
-
-ruta.unshift(inicio);
-
-return {
-    distancia: distancia,
-    ruta: ruta // Cambiamos esto de rutasUtilizadas a ruta
-};
-}
-
 const puntoInicio = "C"; // Cambia el punto de inicio según tus necesidades
-const resultado = algoritmo(data, puntoInicio);
+const resultado = mostrarRuta(datosJSON, puntoInicio);
 console.log("Distancias:", resultado.distancia);
 console.log("Ruta definitiva:", resultado.ruta);
